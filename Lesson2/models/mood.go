5ac98cf8f446ea106e72069817d11cdd13030d4d
millennial <ereskarasal94@gmail.com>
2025-03-10 17:10:39 +0500

    refactor: deleted dev comments

--- Lesson2/models/mood.go
+++ Lesson2/models/mood.go
@@ -6,5 +6,5 @@ type Mood struct {
 	gorm.Model
 	Value  uint8  `json:"value"` // 1 - 10
 	Note   string `json:"note"`
-	UserID uint   `json:"-"` // ???
+	UserID uint   `json:"-"`
 }
--- Lesson2/models/user.go
+++ Lesson2/models/user.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"uniqueIndex;not null" json:"username"` // ???
-	Password string `gorm:"not null" json:"-"`                    // ???
+	Username string `gorm:"uniqueIndex;not null" json:"username"`
+	Password string `gorm:"not null" json:"-"`
 	Moods    []Mood
 }
